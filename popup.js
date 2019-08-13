141173fa5219c0e8a9eda6a0b0575b6fc8cf3bed
Shamil Ataev <shamil.ataev@segment.com>
2019-08-14 00:56:01 +0100

    better working version

--- popup.js
+++ popup.js
@@ -1,43 +1,41 @@
+function copyStringToClipboardA () {
+  // Create new element
+  var el = document.createElement('input');
+  // Set value (string to be copied)
+  el.value = document.getElementById("trackEvent").value;
+  // Set non-editable to avoid focus and move outside of view
+  el.setAttribute('readonly', '');
+  el.style = {position: 'absolute', left: '-9999px'};
+  document.body.appendChild(el);
+  // Select text inside element
+  el.select();
+  // Copy text to clipboard
+  document.execCommand('copy');
+  // Remove temporary element
+  document.body.removeChild(el);
+}
+
+function copyStringToClipboardB() {
+  // Create new element
+  var el = document.createElement('input');
+  // Set value (string to be copied)
+  el.value = document.getElementById("identifyEvent").value;
+  // Set non-editable to avoid focus and move outside of view
+  el.setAttribute('readonly', '');
+  el.style = {position: 'absolute', left: '-9999px'};
+  document.body.appendChild(el);
+  // Select text inside element
+  el.select();
+  // Copy text to clipboard
+  document.execCommand('copy');
+  // Remove temporary element
+  document.body.removeChild(el);
+}
+
 document.addEventListener("DOMContentLoaded", function(event){
     var b = document.getElementById('trackCopy');
+    var c = document.getElementById('blabla');
     b.addEventListener('click', copyStringToClipboardA, false);
-
-      function copyStringToClipboardA () {
-        // Create new element
-        var el = document.createElement('input');
-        // Set value (string to be copied)
-        el.value = document.getElementById("trackEvent").value;
-        // Set non-editable to avoid focus and move outside of view
-        el.setAttribute('readonly', '');
-        el.style = {position: 'absolute', left: '-9999px'};
-        document.body.appendChild(el);
-        // Select text inside element
-        el.select();
-        // Copy text to clipboard
-        document.execCommand('copy');
-        // Remove temporary element
-        document.body.removeChild(el);
-     }
+    c.addEventListener('click', copyStringToClipboardB, false)
 });
 
-document.addEventListener("DOMContentLoaded", function(event){
-  var c = document.getElementById('identifyCopy');
-  c.addEventListener('click', copyStringToClipboardB, false);
-
-    function copyStringToClipboardB () {
-      // Create new element
-      var el = document.createElement('input');
-      // Set value (string to be copied)
-      el.value = document.getElementById("identifyEvent").value;
-      // Set non-editable to avoid focus and move outside of view
-      el.setAttribute('readonly', '');
-      el.style = {position: 'absolute', left: '-9999px'};
-      document.body.appendChild(el);
-      // Select text inside element
-      el.select();
-      // Copy text to clipboard
-      document.execCommand('copy');
-      // Remove temporary element
-      document.body.removeChild(el);
-   }
-});
